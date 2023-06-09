f425bab9cbf29f1aed8b69964af716b0d526158f
Konstantine <kotechkholaria@gmail.com>
2023-06-09 17:40:01 +0400

    added exchange_rates

--- SCR.py
+++ SCR.py
@@ -1,29 +1,30 @@
 import openpyxl
+from exchange_rates import pull_rates
+# def process_excel_task(task_name, file_name):
+#     # Load the Excel workbook
+#     workbook = openpyxl.load_workbook(file_name)
 
-def process_excel_task(task_name, file_name):
-    # Load the Excel workbook
-    workbook = openpyxl.load_workbook(file_name)
+#     # Select the first sheet in the workbook
+#     sheet = workbook.active
 
-    # Select the first sheet in the workbook
-    sheet = workbook.active
+#     if task_name == "sum_column":
+#         # Sum the values in column A and return the result
+#         sum_value = 0
+#         for row in sheet.iter_rows(min_row=1, max_col=1, values_only=True):
+#             sum_value += row[0]
+#         return f"The sum of column A is: {sum_value}"
 
-    if task_name == "sum_column":
-        # Sum the values in column A and return the result
-        sum_value = 0
-        for row in sheet.iter_rows(min_row=1, max_col=1, values_only=True):
-            sum_value += row[0]
-        return f"The sum of column A is: {sum_value}"
+#     elif task_name == "count_rows":
+#         # Count the number of rows in the sheet and return the result
+#         row_count = sheet.max_row
+#         return f"The number of rows in the sheet is: {row_count}"
 
-    elif task_name == "count_rows":
-        # Count the number of rows in the sheet and return the result
-        row_count = sheet.max_row
-        return f"The number of rows in the sheet is: {row_count}"
+#     else:
+#         return "Invalid task name. Please provide a valid task name."
 
-    else:
-        return "Invalid task name. Please provide a valid task name."
-
-# Example usage of the function
-task_name = "sum_column"
-file_name = "example.xlsx"
-result = process_excel_task(task_name, file_name)
-print(result)
+# # Example usage of the function
+# task_name = "sum_column"
+# file_name = "example.xlsx"
+# result = process_excel_task(task_name, file_name)
+# print(result)
+pull_rates()
--- exchange_rates.py
+++ exchange_rates.py
@@ -1,13 +1,45 @@
-import openpyxl
-import pandas as pd
-path = r"/content/drive/MyDrive/FILES_FOR_COLAB/SNG.xlsx"
-wb = openpyxl.load_workbook(path)
+import requests
+from openpyxl import load_workbook
+from xml.etree import ElementTree as ET
+def pull_rates():
+  url = "https://services.nbg.gov.ge/Rates/Service.asmx"
 
-wbsheet = wb.active
+  headers = {
+      'Content-Type': 'text/xml; charset=utf-8',
+      'SOAPAction': 'http://www.nbg.ge/GetCurrentRates',
+  }
 
-#get exchange_rate from csv file (nbg.gov.ge link)
-csv_url = r"https://nbg.gov.ge/fm/%E1%83%A1%E1%83%A2%E1%83%90%E1%83%A2%E1%83%98%E1%83%A1%E1%83%A2%E1%83%98%E1%83%99%E1%83%90/exchange_rates/geo/official-daily-exchange-ratesgeo.xlsx?v=o0wcq"
-df = pd.read_excel(csv_url)
+  body = """<?xml version="1.0" encoding="utf-8"?>
+  <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+    <soap:Body>
+      <GetCurrentRates xmlns="http://www.nbg.ge/">
+        <Currencies></Currencies>
+      </GetCurrentRates>
+    </soap:Body>
+  </soap:Envelope>"""
 
+  response = requests.post(url, headers=headers, data=body)
+  root = ET.fromstring(response.content)
 
-df.head(10)
+  namespace = {'soap': 'http://schemas.xmlsoap.org/soap/envelope/', 'nbg': 'http://www.nbg.ge/'}
+
+  rates = {}
+  for rate in root.findall('.//nbg:GetCurrentRatesResult/nbg:CurrencyRate', namespace):
+      code = rate.find('nbg:Code', namespace).text
+      quantity = rate.find('nbg:Quantity', namespace).text
+      rate_value = rate.find('nbg:Rate', namespace).text
+      rates[code] = (quantity, rate_value)
+
+  print(rates)
+
+  # Load the existing workbook
+  wb = load_workbook(r'C:\Users\Kchkholaria\Desktop\Project_SCR\working_files\SNG.xlsx')
+  ws = wb.active
+
+  # Write rates to specified cells
+  ws['B16'] = float(rates['USD'][1]) / int(rates['USD'][0])  # USD rate value
+  ws['B17'] = float(rates['EUR'][1]) / int(rates['EUR'][0])  # EUR rate value
+  ws['B18'] = float(rates['RUB'][1]) / int(rates['RUB'][0])  # RUB rate value
+
+  # Save the workbook to a file
+  wb.save(r'C:\Users\Kchkholaria\Desktop\Project_SCR\working_files\SNG.xlsx')
